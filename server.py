54be4326c48eff8bcb8d72bbd61dcf4b75b4fcf2
Fatih Kabak <fatihkabak39@hotmail.com>
2022-04-07 03:26:31 +0300

    Update & Add register, login functionality

--- server.py
+++ server.py
@@ -1,127 +1,151 @@
+from forms import RegisterForm, LoginForm
 from flask import Flask, session, request
-from sqlite_context import SqliteContext
+from flask_sqlalchemy import SQLAlchemy
 from passlib.hash import sha512_crypt
+from core.validation import *
 from compress import Compress
 from functools import wraps
-import db_operations
 from log import Log
-import json
+import jwt
 import os
 
 
 class App(Flask):
     def process_response(self, response):
         response.headers['server'] = 'KlinzBlog'
-        super(App, self).process_response(response)
+        response = super(App, self).process_response(response)
         return response
 
 
 app = App(__name__)
 app.secret_key = os.urandom(16)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+db = SQLAlchemy(app)
 Compress(app)
 Log(app)
 
 
-def jsonify(needs_json=False):
-    """ Wrapper to transform data into json.
+# Database ORMs
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(150))
 
-    Also checks if the function needs json to perform.
-
-    Args:
-        needs_json (bool, optional): Checks if request contains json data. Defaults to False.
-    """
-    def main_wrapper(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            if needs_json and request.json is None:
-                return {'message': 'Needs json'}
-            return json.dumps(func(*args, **kwargs))
-        return wrapper
-    return main_wrapper
+    def save(self, db: SQLAlchemy = db):
+        db.session.add(self)
+        db.session.commit()
 
 
-@app.route("/", endpoint='index')
-@jsonify()
+def token_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        token = request.headers.get('x-access-token', None)
+
+        if not token:
+            return ErrorResponse('Token is missing.'), 400
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            user = User.query\
+                .filter_by(id=data['id'])\
+                .first()
+            return f(user, *args, **kwargs)
+        except Exception:
+            return ErrorResponse('Token is invalid'), 401
+
+    return wrapper
+
+
+def needs_json(func):
+    ''' Checks if the request's json body is ok. '''
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if request.json is None:
+            return ErrorResponse('Invalid json body.')
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return ErrorResponse('Not found.'), 404
+
+
+@app.route('/')
 def index():
     return {'message': 'Hello, World!'}
 
 
-@app.route('/post', methods=['POST'], endpoint='form_test')
-@jsonify(needs_json=True)
+@app.route('/post', methods=['POST'])
 def form_test():
     print(request.data, request.form, request.get_data(), request.json)
+    return {'Data': str(request.data), 'Form': request.form, 'Get_Data': str(request.get_data()), 'JSON': request.json}
     return {'message': 'Ok'}
+
+
+@app.route('/form', methods=['POST'])
+def form():
+    form = LoginForm(json_data=request.json, **request.form)
+    if not form.validate():
+        return ErrorResponse('Invalid form body'), 400
+    return {'Username': form.username, 'Password': form.password}
 
 
 def login_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        if "logged_in" in session:
+        if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
-            return redirect(url_for("login"))
+            return ErrorResponse('Unauthorized'), 401
 
     return wrapper
 
 
-@app.route("/register", methods=["POST"], endpoint='register')
-@jsonify
+@app.route('/register', methods=['POST'])
 def register():
-    form = RegisterForm(request.form)
+    form = RegisterForm(json_data=request.json, **request.form)
 
-    if request.method == "POST" and form.validate():
-        name = form.name.data
-        username = form.username.data
-        email = form.email.data
-        password = sha512_crypt.hash(form.password.data)
+    if not form.validate():
+        return ErrorResponse('Invalid form body'), 400
 
-        query = "INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)"
+    username = form.username
+    password = sha512_crypt.hash(form.password)
 
-        with SqliteContext() as cursor:
-            cursor.execute(query, (name, email, username, password))
+    if User.query.filter_by(username=username).first():
+        return ErrorResponse('User exists.'), 202
 
-        flash(message="Başarıyla Kayıt Oldunuz...", category="success")
-
-        return redirect(url_for("login"))
-    else:
-        return render_template("register.html", form=form)
+    user = User(username=username, password=password)
+    user.save()
+    return SuccessResponse('Successfully registered.'), 201
 
 
-@app.route("/login", methods=["GET", "POST"], endpoint='login')
-@jsonify
+@app.route('/login', methods=['POST'])
 def login():
-    form = LoginForm(request.form)
+    form = LoginForm(json_data=request.json, **request.form)
 
-    if request.method == "POST":
-        username = form.username.data
-        form_pass = form.password.data
+    if not form.validate():
+        return ErrorResponse('Invalid form body'), 400
 
-        query = "SELECT * FROM users WHERE username = %s"
+    username = form.username
+    form_pass = form.password
 
-        with SqliteContext() as cursor:
-            cursor.execute(query, (username, ))
-            data = cursor.fetchone()
-
-        if data and len(data) > 0:
-            real_pass = data["password"]
-            if sha512_crypt.verify(form_pass, real_pass):
-                flash("Başarıyla Giriş Yaptınız. Yönlendiriliyorsunuz...",
-                      category="success")
-
-                session["logged_in"] = True
-                session["username"] = username
-
-                return redirect(url_for("index"))
-
-        flash(
-            "Kullanıcı Adı Veya Şifreniz Hatalı! Lütfen Tekrar Deneyiniz...",
-            category="danger")
-        return redirect(url_for("login"))
-
-    else:
-        return render_template("login.html", form=form)
+    user = User.query.filter_by(username=username).first()
+    if user:
+        if sha512_crypt.verify(form_pass, user.password):
+            token = jwt.encode({
+                'id': user.id, 'username': user.username
+            }, app.config['SECRET_KEY'])
+            return SuccessDataResponse({'token': token}, 'Login successful.')
+        return ErrorResponse('Debug[pass] Invalid credentials.')
+    return ErrorResponse('Debug[user] Invalid credentials.')
+    # if data and len(data) > 0:
+    #     real_pass = data['password']
+    #     if sha512_crypt.verify(form_pass, real_pass):
+    #         pass
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
