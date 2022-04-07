c9c3f7e737f926e11cfbc4a8a5f45a7abc3f76fc
Fatih Kabak <fatihkabak39@hotmail.com>
2022-04-07 03:26:07 +0300

    Improve forms

--- forms/forms.py
+++ forms/forms.py
@@ -3,16 +3,18 @@ import json
 
 class Form:
     def __init__(self, json_data=None, **kwargs) -> None:
-        if json_data:
-            self.data = json.loads(json_data)
-        if kwargs:
-            self.data = kwargs
+        self.data = json_data or kwargs
 
     def validate(self):
         for field in self.fields:
             if not field in self.data:
                 return False
+        self.set_attributes()
         return True
+
+    def set_attributes(self):
+        for i in self.fields:
+            setattr(self, i, self.data.get(i))
 
 
 class LoginForm(Form):
