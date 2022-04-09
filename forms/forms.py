353c1e75801d7032a91b75da222d58030dec4685
Fatih Kabak <fatihkabak39@hotmail.com>
2022-04-09 04:40:02 +0300

    Update form processing

--- forms/forms.py
+++ forms/forms.py
@@ -1,8 +1,12 @@
+from json import loads
+
+
 class Form:
     def __init__(self, json_data=None, **kwargs) -> None:
         self.data = json_data or kwargs
 
     def validate(self):
+        print(self.fields, self.data)
         for field in self.fields:
             if not field in self.data:
                 return False
@@ -10,8 +14,11 @@ class Form:
         return True
 
     def set_attributes(self):
+        """ Uses an exceptional case for boolean values. """
         for i in self.fields:
-            setattr(self, i, self.data.get(i))
+            data = self.data.get(i)
+            setattr(self, i, loads(data)) if data in (
+                'false', 'true') else setattr(self, i, data)
 
 
 class RegisterForm(Form):
