343e7a97cccf101c7986c88205a93f4b91354fb1
Fatih Kabak <fatihkabak39@hotmail.com>
2022-05-24 02:15:14 +0300

    Update responses

--- core/validation/__init__.py
+++ core/validation/__init__.py
@@ -1,6 +1,6 @@
-from .responses import SuccessResponse, SuccessDataResponse, ErrorDataResponse, ErrorResponse
+from .responses import SuccessResponse, SuccessDataResponse, ErrorDataResponse, ErrorResponse, Response
 from .validations import run_checks
 from . import messages
 
-__all__ = ['messages', 'SuccessResponse', 'SuccessDataResponse',
+__all__ = ['messages', 'SuccessResponse', 'SuccessDataResponse', 'Response',
            'ErrorResponse', 'ErrorDataResponse', 'run_checks']
--- core/validation/responses.py
+++ core/validation/responses.py
@@ -14,18 +14,20 @@ class Response(dict):
 
 class SuccessResponse(Response):
     def __init__(self, message=None):
-        super(SuccessResponse, self).__init__(success=True, message=message)
+        super().__init__(success=True, message=message)
 
 
 class ErrorResponse(Response):
     def __init__(self, message=None):
-        super(ErrorResponse, self).__init__(success=False, message=message)
+        super().__init__(success=False, message=message)
 
 
 class DataResponse(Response):
     def __init__(self, success, data=None, message=None):
-        super(DataResponse, self).__init__(success=success, message=message)
+        super().__init__(success=success, message=message)
         self['data'] = data
+        if hasattr(data, 'to_json'):
+            self['data'] = data.to_json()
 
     def __repr__(self):
         return f"DataResponse(success={self['success']}, " \
@@ -34,11 +36,11 @@ class DataResponse(Response):
 
 class SuccessDataResponse(DataResponse):
     def __init__(self, data=None, message=None):
-        super(SuccessDataResponse, self).__init__(
+        super().__init__(
             success=True, data=data, message=message)
 
 
 class ErrorDataResponse(DataResponse):
     def __init__(self, data=None, message=None):
-        super(ErrorDataResponse, self).__init__(
+        super().__init__(
             success=False, data=data, message=message)
